84fa1eaa9a427255adb75d17609a7526aaaf2112
Sumit Gogia <sumit.y.gogia@gmail.com>
2018-12-27 21:28:04 -0800

    add file pasting fzf functions

--- fzf/main.zsh
+++ fzf/main.zsh
@@ -5,14 +5,51 @@ export FZF_DEFAULT_OPTS="--height 40% --border"
 
 fzf-paste-file() {
   local selected
-  if selected=$(fd | fzf); then
+  if selected=$(fd -Lp | fzf); then
     BUFFER="$LBUFFER$selected $RBUFFER"
   fi
   zle redisplay
   CURSOR=$(($CURSOR + $#selected + 2))
 }
 zle -N fzf-paste-file
-bindkey '^l' fzf-paste-file
+bindkey 'fpf' fzf-paste-file
+
+fzf-paste-file-all() {
+  local selected
+  if selected=$(fd -LHIp | fzf); then
+    BUFFER="$LBUFFER$selected $RBUFFER"
+  fi
+  zle redisplay
+  CURSOR=$(($CURSOR + $#selected + 2))
+}
+zle -N fzf-paste-file-all
+bindkey 'fpa' fzf-paste-file-all
+
+fzf-paste-file-home() {
+  local selected
+  if selected=$(fd -Lp '' $HOME | fzf); then
+    BUFFER="$LBUFFER$selected $RBUFFER"
+  fi
+  zle redisplay
+  CURSOR=$(($CURSOR + $#selected + 2))
+}
+zle -N fzf-paste-file-home
+bindkey 'fph' fzf-paste-file-home
+
+fzf-paste-file-git() {
+  local selected
+  local gitroot
+  gitroot=$(git rev-parse --show-toplevel)
+  if selected=$(git ls-files $gitroot | fzf); then
+    BUFFER="$LBUFFER$selected $RBUFFER"
+  fi
+  zle redisplay
+  CURSOR=$(($CURSOR + $#selected + 2))
+}
+zle -N fzf-paste-file-git
+bindkey 'fpg' fzf-paste-file-git
+
+# fzf-paste-file-project() {}
 
 # search projects
 # search directories
