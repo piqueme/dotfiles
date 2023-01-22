1d57963f19d218b782386c0fba07be9ff9e86751
Sumit Gogia <sumit.y.gogia@gmail.com>
2023-01-21 19:06:42 -0800

    clean up bazel fzf helpers

--- fzf/bazel.zsh
+++ fzf/bazel.zsh
@@ -1,10 +1,14 @@
 # UTIL
-check-in-bazel-workspace() {
+warn() {
+  local yellow='\033[1;33m'
+  echo "${yellow}[WARN] $@"
+}
+
+_check-in-bazel-workspace() {
   bazel info > /dev/null 2>&1
   if [[ "$?" -ne 0 ]]; then
     echo ""
-    local yellow='\033[1;33m'
-    echo "${yellow}[ERROR] Cannot search Bazel targets. Not in a Bazel workspace."
+    warn "Cannot search Bazel targets. Not in a Bazel workspace."
     echo ""
     if [[ -n "$ZLE_STATE" ]]; then
       echo ""
@@ -13,28 +17,50 @@ check-in-bazel-workspace() {
   fi
 }
 
-fzf-get-bazel-target() {
+_fzf-get-bazel-target() {
+  local no_build=false
+  local no_test=false
+
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --no_build)
+        no_build=true
+        shift
+        ;;
+      --no_test)
+        no_test=true
+        shift
+        ;;
+      *)
+        warn "Did not recognize argument $1 for fzf-get-bazel-targets"
+        return 1
+        ;;
+    esac
+  done
+
+  local query
+
+  if [[ $no_test == "true" ]] && [[ $no_build == "false" ]]
+  then
+    query="//... - tests(//...)"
+  elif [[ $no_test == "false" ]] && [[ $no_build == "false" ]]
+  then
+    query="//..."
+  elif [[ $no_test == "false" ]] && [[ $no_build == "true" ]]
+  then
+    query="tests(//...)"
+  else
+    warn "Cannot ignore both tests and build targets -> empty set"
+    return 1
+  fi
+
   local targets target
-  targets=$(bazel query "//..." 2>/dev/null) &&
+  targets=$(bazel query "$query" 2>/dev/null) &&
   target=$(echo "$targets" | fzf +m +s) &&
   echo "$target"
 }
 
-fzf-get-bazel-build-targets() {
-  local targets target
-  targets=$(bazel query "//... - tests(//...)" 2>/dev/null) &&
-  target=$(echo "$targets" | fzf +m +s) &&
-  echo "$target"
-}
-
-fzf-get-bazel-test-targets() {
-  local targets target
-  targets=$(bazel query "tests(//...)" 2>/dev/null) &&
-  target=$(echo "$targets" | fzf +m +s) &&
-  echo "$target"
-}
-
-fzf-get-bazel-package() {
+_fzf-get-bazel-package() {
   local packages package
   packages=$(bazel query "//..." --output package 2>/dev/null) &&
   package=$(echo "$packages" | fzf +m +s) &&
@@ -42,106 +68,107 @@ fzf-get-bazel-package() {
 }
 
 # FUNCTIONS
-fzf-paste-bazel-target() {
-  check-in-bazel-workspace &&
-  target=$(fzf-get-bazel-target) &&
+_fzf-paste-bazel-target() {
+  _check-in-bazel-workspace &&
+  target=$(_fzf-get-bazel-target) &&
   BUFFER=$LBUFFER$target $RBUFFER &&
   zle redisplay
   CURSOR=$(($CURSOR + $#target + 2))
 }
-zle -N fzf-paste-bazel-target
-bindkey '^b' fzf-paste-bazel-target
+zle -N _fzf-paste-bazel-target
+bindkey '^b' _fzf-paste-bazel-target
+
+_fzf-paste-bazel-package() {
+  _check-in-bazel-workspace &&
+  package=$(_fzf-get-bazel-package) &&
+  BUFFER=$LBUFFER$package $RBUFFER &&
+  zle redisplay
+  CURSOR=$(($CURSOR + $#package + 2))
+}
+zle -N _fzf-paste-bazel-package
+bindkey '^o' _fzf-paste-bazel-package
 
 # core
-fzf-query-bazel-target-deps() {
-  check-in-bazel-workspace &&
-  local target=$(fzf-get-bazel-target) &&
-  bazel query "deps($target)"
-}
-# widget
-fzf-query-bazel-target-deps-widget() {
-  fzf-query-bazel-target-deps &&
-  zle reset-prompt
-}
-# alias
-bd() {
-  fzf-query-bazel-target-deps
-}
-zle -N fzf-query-bazel-target-deps-widget
-bindkey ';bd' fzf-query-bazel-target-deps-widget
-
-# core
-fzf-query-bazel-target-rdeps() {
-  check-in-bazel-workspace &&
-  local target=$(fzf-get-bazel-target) &&
-  bazel query "rdeps("//...", $target)"
-}
-# widget
-fzf-query-bazel-target-rdeps-widget() {
-  fzf-query-bazel-target-rdeps &&
-  zle reset-prompt
-}
-# alias
-br() {
-  fzf-query-bazel-target-rdeps
-}
-zle -N fzf-query-bazel-target-rdeps-widget
-bindkey ';br' fzf-query-bazel-target-rdeps-widget
-
-# core
-fzf-build-bazel-target() {
-  check-in-bazel-workspace &&
-  target=$(fzf-get-bazel-build-targets) &&
+_fzf-build-bazel-target() {
+  _check-in-bazel-workspace &&
+  target=$(fzf-get-bazel-target --no_test) &&
   bazel build "$target"
 }
 # widget
-fzf-build-bazel-target-widget() {
-  fzf-build-bazel-target &&
+_fzf-build-bazel-target-widget() {
+  _fzf-build-bazel-target &&
   zle reset-prompt
 }
-# alias
-bb() {
-  fzf-build-bazel-target
-}
-zle -N fzf-build-bazel-target-widget
-bindkey ';ba' fzf-build-bazel-target-widget
+zle -N _fzf-build-bazel-target-widget
+bindkey ';be' _fzf-build-bazel-target-widget
 
 # core
-fzf-test-bazel-target() {
-  check-in-bazel-workspace &&
-  target=$(fzf-get-bazel-test-targets) &&
+_fzf-test-bazel-target() {
+  _check-in-bazel-workspace &&
+  target=$(_fzf-get-bazel-target --no_build) &&
   bazel test "$target"
 }
 # widget
-fzf-test-bazel-target-widget() {
-  fzf-test-bazel-target &&
+_fzf-test-bazel-target-widget() {
+  _fzf-test-bazel-target &&
   zle reset-prompt
 }
-# alias
-bt() {
-  fzf-test-bazel-target
-}
-zle -N fzf-test-bazel-target-widget
-bindkey ';bt' fzf-test-bazel-target-widget
+zle -N _fzf-test-bazel-target-widget
+bindkey ';bt' _fzf-test-bazel-target-widget
 
 # core
-fzf-build-bazel-package() {
-  check-in-bazel-workspace &&
-  package=$(fzf-get-bazel-package) &&
-  bazel build "${package}:all"
-}
-# alias
-bbp() {
-  fzf-build-bazel-package
-}
-
-# core
-fzf-test-bazel-package() {
-  check-in-bazel-workspace &&
-  package=$(fzf-get-bazel-package) &&
+_fzf-test-bazel-package() {
+  _check-in-bazel-workspace &&
+  package=$(_fzf-get-bazel-package) &&
   bazel test "${package}:all"
 }
-# alias
-btp() {
-  fzf-test-bazel-package
+# widget
+_fzf-test-bazel-package-widget() {
+  _fzf-test-bazel-package &&
+  zle reset-prompt
+}
+# binding
+zle -N _fzf-test-bazel-package-widget
+bindkey ';bp' _fzf-test-bazel-package-widget
+
+_bazel-internal-filter() {
+  echo "filter(\"^//.*\", $@)"
+}
+alias bif=_bazel-internal-filter
+
+_bazel-external-filter() {
+  echo "filter(\"^[^/].*\", $@)"
+}
+alias bef=_bazel-external-filter
+
+alias bb="bazel build"
+alias bt="bazel test"
+alias bq="bazel query"
+
+bqi() {
+  local query
+  query=$(_bazel-internal-filter "${@}")
+  bazel query "$query"
+}
+
+bqe() {
+  local query
+  query=$(_bazel-external-filter "${@}")
+  bazel query "$query"
+}
+
+bd() {
+  bazel query "deps(${@})"
+}
+
+bds() {
+  bazel query "deps(${@}, 1)"
+}
+
+br() {
+  bazel query "rdeps(\"//...\", ${@})"
+}
+
+brs() {
+  bazel query "rdeps(\"//...\", ${@}, 1)"
 }
